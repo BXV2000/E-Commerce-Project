6fc23ae5c4a2d286709d75678b01ab5bd891b221
BXV2000 <82429306+BXV2000@users.noreply.github.com>
2022-06-17 13:25:58 +0700

    Add VegetableController (get , get by id, post)

---
+++ ECommerce.API/Controllers/VegetableController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ECommerce.API.Models;
+using ECommerce.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VegetableController : ControllerBase
+    {
+        private readonly ECommerceDbContext _context;
+
+        public VegetableController(ECommerceDbContext context)
+        {
+            _context = context;
+        }
+
+        //Get all Category
+        [HttpGet]
+        public async Task<ActionResult<List<Vegetable>>> GetAllVegetable()
+        {
+            return Ok(await _context.Vegetables.ToListAsync());  
+        }
+
+        //Get one Category
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Vegetable>> GetOneVegetable(int id)
+        {
+            var Vegetable = await _context.Vegetables.FindAsync(id);
+            if (Vegetable == null) return BadRequest("Vegetable not found :(");
+            return Ok(Vegetable);    
+        }
+
+        //Post Category
+        [HttpPost]
+        public async Task<ActionResult<List<Vegetable>>> PostVegetable(Vegetable info)
+        {
+            _context.Vegetables.Add(info);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Vegetables.ToListAsync());
+        }
+
+
+    }
+}
