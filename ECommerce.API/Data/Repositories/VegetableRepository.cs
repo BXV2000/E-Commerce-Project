d96b18f53964c3e7f5c730f58366f15772442826
BXV2000 <82429306+BXV2000@users.noreply.github.com>
2022-06-28 21:56:38 +0700

    Add Vegetable interface Implement Interface to Vegetable Repository

---
+++ ECommerce.API/Data/Repositories/VegetableRepository.cs
@@ -0,0 +1,36 @@
+﻿using ECommerce.API.Interfaces;
+using ECommerce.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.API.Data.Repositories
+{
+    public class VegetableRepository : IVegetableRepository
+    {
+        private readonly ECommerceDbContext _context;
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Vegetable>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Vegetable> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Vegetable> PostAsync(Vegetable image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Vegetable> PutAsync(int id, Vegetable image)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ECommerce.API/Interfaces/IVegetableRepository.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.API.Models;
+
+namespace ECommerce.API.Interfaces
+{
+    public interface IVegetableRepository
+    {
+        Task<List<Vegetable>> GetAsync();
+        Task<Vegetable> GetByIdAsync(int id);
+        Task<Vegetable> PostAsync(Vegetable image);
+        Task<Vegetable> PutAsync(int id,Vegetable image);
+        Task DeleteAsync(int id);
+    }
+}
