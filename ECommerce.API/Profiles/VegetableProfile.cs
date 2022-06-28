a2dcf88c1e3b06d5d8b338642591efd7b669b497
BXV2000 <82429306+BXV2000@users.noreply.github.com>
2022-06-28 21:52:30 +0700

    Create Vegetable DTO Create Vegetable Profile

---
+++ ECommerce.API/Profiles/VegetableProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ECommerce.API.Models;
+using ECommerce.SharedDataModels;
+
+namespace ECommerce.API.Profiles
+{
+    public class VegetableProfile : Profile
+    {
+        public VegetableProfile()
+        {
+            CreateMap<Vegetable, VegetableDTO>();
+            CreateMap<VegetableDTO, Vegetable>()
+                .ForMember(dest => dest.Id, o => o.Ignore())
+                .ForMember(dest => dest.IsDeleted, o => o.Ignore());
+        }
+    }
+}
---
+++ ECommerce.SharedDataModels/VegetableDTO.cs
@@ -0,0 +1,22 @@
+﻿using ECommerce.API.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.SharedDataModels
+{
+    public class VegetableDTO
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public DateTime MFGDate { get; set; }
+        public DateTime EXPDate { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public bool IsDeleted { get; set; }
+        public List<ImageDTO> ImageList { get; set; }
+    }
+}
