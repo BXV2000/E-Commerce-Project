1c1b982542a65fffd5c4326b47f3ac549a8ccb68
BXV2000 <82429306+BXV2000@users.noreply.github.com>
2022-07-05 15:42:44 +0700

    complete product admin layout

--- ECommerce.Admin/ClientApp/src/components/Vegetable.js
+++ ECommerce.Admin/ClientApp/src/components/Vegetable.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import './testStyles.css'
+import '../css/Vegetable.css'
+
 
 let baseURL = "https://localhost:7024/api/"
 export class Vegetable extends Component {
@@ -64,27 +66,40 @@ export class Vegetable extends Component {
     render() {
         const { vegetables } = this.state;
         return (
-            <div>
-                <h2>Vegetable List</h2>
-                <Link to="/vegetable-create" className="create-button"><i class="fa-solid fa-circle-plus"></i> Create</Link>
+            <div className="product-container">
+
+                <h2>PRODUCTS</h2>
+                <Link to="/vegetable-create" className="button"><i class="fa-solid fa-circle-plus"></i> CREATE</Link>
                 <table >
-                    <thead>
+                    <thead className="table-header">
                         <tr>
                             <th>Id</th>
-                            <th>Category Id</th>
+                            <th>Category</th>
+                            <th>Name</th>
                             <th>Price</th>
+                            <th>Stock</th>
                             <th></th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody className="table-body">
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                            <td>5</td>
+                            <td className="button-container"><button className="button"><i class="fa-solid fa-pen"></i> Edit</button><button className="button delete-button"><i class="fa-solid fa-trash"></i> Delete</button></td>
+                        </tr>
                         {vegetables.map(vegetable => (
                             <tr key={vegetable.id}>
                                 <td>{vegetable.id}</td>
                                 <td>{vegetable.categoryId}</td>
+                                <td>{vegetable.name}</td>
                                 <td>{vegetable.price}</td>
-                                <td>
-                                    <button value={vegetable.id} onClick={event => this.deleteVegetable(event.target.value)}>Delete</button>
-                                    {/*<button value={vegetable.id} onClick={event => this.getImageInfo(event.target.value)}>Edit</button>*/}
+                                <td>{vegetable.stock}</td>
+                                <td className="button-container">
+                                    {/*<button className="button" value={vegetable.id} onClick={event => this.getVegetableInfo(event.target.value)}> <i class="fa-solid fa-pen"></i> Edit</button>*/}
+                                    <button className="button delete-button" value={vegetable.id} onClick={event => this.deleteVegetable(event.target.value)}><i class="fa-solid fa-trash"></i> Delete</button>
                                     {/*<button value={vegetable.id} >Delete</button>*/}
                                     {/*<button value={vegetable.id} >Edit</button>*/}
                                 </td>
