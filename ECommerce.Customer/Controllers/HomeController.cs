cbb28f5a5262e28b2e965d1423bb5e70ace502f4
BXV2000 <82429306+BXV2000@users.noreply.github.com>
2022-07-07 11:02:36 +0700

    working on customer side

--- ECommerce.Customer/Controllers/HomeController.cs
+++ ECommerce.Customer/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿using ECommerce.API.DTOs;
+using ECommerce.API.Services;
 using ECommerce.Customer.Models;
 using ECommerce.Customer.Services;
 using ECommerce.SharedDataModels;
+using ECommerce.SharedDataModels.Authenticate;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Refit;
 using System.Diagnostics;
@@ -14,12 +17,32 @@ namespace ECommerce.Customer.Controllers
         List<VegetableDTO> viewVegetables = new List<VegetableDTO>();
         IVegetableService vegetableService = RestService.For<IVegetableService>("https://localhost:7024/api");
         private readonly ILogger<HomeController> _logger;
+        private IUserService _userService;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(IUserService userService)
         {
-            _logger = logger;
+            _userService = userService;
         }
 
+        public IActionResult Login() => View();
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(AuthenticateRequestDTO authenticateRequest)
+        {
+            try
+            {
+                var response = _userService.Authenticate(authenticateRequest);
+                if (response == null) return RedirectToAction("Error");
+                HttpContext.Session.SetString("Token", response.Token);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return RedirectToAction("Error");
+            }
+        }
         public async Task<IActionResult> Index()
         {
             try
