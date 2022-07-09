7bf924b2b364cbde9ec5863669e7e0b4c79c48cb
BXV2000 <82429306+BXV2000@users.noreply.github.com>
2022-07-09 16:05:43 +0700

    Complete shop page Complete product detail page

--- ECommerce.Customer/Controllers/ShopController.cs
+++ ECommerce.Customer/Controllers/ShopController.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerce.Customer.Services;
+using ECommerce.SharedDataModels;
+using Microsoft.AspNetCore.Mvc;
+using Refit;
 
 namespace ECommerce.Customer.Controllers
 {
     public class ShopController : Controller
     {
-        public IActionResult Index()
+        VegetableDTO viewVegetable = new VegetableDTO();
+        List<VegetableDTO> viewVegetables = new List<VegetableDTO>();
+        IVegetableService vegetableService = RestService.For<IVegetableService>("https://localhost:7024/api");
+        private readonly ILogger<HomeController> _logger;
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+            try
+            {
+                viewVegetables = await vegetableService.GetVegetables();
+                return View(viewVegetables);
+            }
+            catch
+            {
+                return RedirectToAction("Error");
+            }
+        }
+        public async Task<IActionResult> Detail(int id)
+        {
+            try
+            {
+                viewVegetable = await vegetableService.GetVegetable(id);
+                return View(viewVegetable);
+            }
+            catch
+            {
+                return RedirectToAction("Error");
+            }
         }
     }
 }
